3a7d9d1423a23cf43052d08b7eb24de8bab1959d
Evgenii Popov <jekson87@me.com>
2019-05-08 22:18:26 +0300

    lint faults fixed

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,11 +4,11 @@ import program from 'commander';
 import { version, description } from '../../package.json';
 
 program
-    .version(version)
-    .description(description)
-    .arguments('<firstConfig>')
-    .arguments('<secondConfig>')
-    .option('-f, --format [type]','Output format')
-    .parse(process.argv);
+  .version(version)
+  .description(description)
+  .arguments('<firstConfig>')
+  .arguments('<secondConfig>')
+  .option('-f, --format [type]', 'Output format')
+  .parse(process.argv);
 
 if (!program.args.length) program.help();
